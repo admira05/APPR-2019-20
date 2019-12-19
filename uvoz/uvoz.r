cf75e9725766eeaa14ea637641eb45507bc90aad
Admira Abdić <admira.abdic@student.fmf.uni-lj.si>
2019-12-19 13:20:18 +0100

    urejanje tabel, prvi del

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -4,18 +4,19 @@ sl <- locale("sl", decimal_mark=",", grouping_mark=".")
 
 
 
-data <- read_csv2("podatki/tujci.namen.csv",locale=locale(encoding="Windows-1250"), skip = 2, na=c("", "...") )
-colnames(data)[1] <- "drzava"
+data <- read_csv2("podatki/tujci.namen.csv",locale=locale(encoding="Windows-1250"), skip = 2, na=c("", "...", "z") ) %>%
+  rename(drzava=`DRŽAVA DRŽAVLJANSTVA`) %>%
+  gather(key="leto.razlog", value="stevilo", -drzava) %>%
+  separate(leto.razlog, c("leto", "razlog"), "(?<=[0-9]) ") %>%
+  mutate(leto=parse_number(leto))
+#colnames(data)[1] <- "drzava"
 
 data1 <- read_csv2("podatki/ods_st_akt.csv",locale=locale(encoding="Windows-1250"), skip = 2, na=c("", "...") )
 
-
-
 data2 <- read_csv2("podatki/notranje.selitve.csv",locale=locale(encoding="Windows-1250"), skip = 2,  na=c("", "..."))
-#colnames(data2) <- c("letnice", "regija" , "odseljenji", "priseljeni", "vrednost")
 
 data3 <- read_csv2("podatki/pris.odselj.csv",locale=locale(encoding="Windows-1250"), skip = 2, na=c("", "...") )
-#colnames(data3) <- c("letnice", "drzava", "odseljeni", "priseljeni", "spol", "vrednost")
+
 
 a <- gather(data,-drzava, key = leto_namen, value = stevilo)
 
